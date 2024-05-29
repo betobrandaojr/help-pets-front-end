ded3a63aec5cdea521b54799505f11c914b80c2a
Thiago Moura <ago10_mariano@yahoo.com.br>
2024-05-29 17:45:03 -0300

    Project configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,27 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideNzIcons } from './icons-provider';
-import { pt_BR, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
 import pt from '@angular/common/locales/pt';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideNzConfig } from 'ng-zorro-antd/core/config';
+import { provideNzI18n, pt_BR } from 'ng-zorro-antd/i18n';
+import { routes } from './app.routes';
+import { ngZorroConfig } from './config/zorro-config.const';
+import { provideNzIcons } from './icons-provider';
 
 registerLocaleData(pt);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideNzIcons(), provideNzI18n(pt_BR), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideNzIcons(),
+    provideNzConfig(ngZorroConfig),
+    provideNzI18n(pt_BR),
+    importProvidersFrom(FormsModule),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+  ],
 };
---
+++ src/app/config/zorro-config.const.ts
@@ -0,0 +1,12 @@
+import { NzConfig } from 'ng-zorro-antd/core/config';
+
+export const ngZorroConfig: NzConfig = {
+  form: {
+    nzAutoTips: {
+      'pt-br': {
+        required: 'O campo é obrigatório',
+        email: 'Deve ser um email válido',
+      },
+    },
+  },
+};
